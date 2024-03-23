1fc11426ebf1c3860bfbb7091db08ae0e6478595
prashasth nair <prashasthnair1@gmail.com>
2024-03-23 18:55:03 +0530

    Bug fix

--- api.js
+++ api.js
@@ -1,21 +1,17 @@
 const express = require("express");
 const yahooFinance = require("yahoo-finance2").default;
-const router = express.Router()
 const app = express();
-router.get('/', (req, res) => {
-     
-    res.status(200).send({
-        status:200,
-        message:"server is up and running"
-      })
-  
+app.get("/", (req, res) => {
+  res.status(200).send({
+    status: 200,
+    message: "server is up and running",
   });
+});
 
-
-  router.get("/search", async (req, res) => {
+app.get("/search", async (req, res) => {
   try {
     var query = req.query.q;
-    
+
     const results = await yahooFinance.search(query);
     res.status(200).send({
       results: results,
@@ -25,23 +21,20 @@ router.get('/', (req, res) => {
   }
 });
 
-router.get("/details",async(req,res)=>{
-    try{
-        var name = req.query.name;
-        const quote  = await yahooFinance.quote(name);
-        res.status(200).send(quote);
-    }catch (error) {
-        res.status(404).send(error);
-      }
-})
-
-
+app.get("/details", async (req, res) => {
+  try {
+    var name = req.query.name;
+    const quote = await yahooFinance.quote(name);
+    res.status(200).send(quote);
+  } catch (error) {
+    res.status(404).send(error);
+  }
+});
 
 // app.get("/api/course/:id", (req, res) => {
 //   res.send(req.params.id); //req.params.id to get :id value
 //   // res.send(req.query) //req.query to get ?sortBy=name values
 // });
-
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () =>
