c331e6749b3a6ad15a1d5c0348de758e0ff362df
priyanshuverma-dev <priyanshu.creator.dev@gmail.com>
2024-03-30 19:45:14 +0530

    Add express server with routes and CORS support

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+import routes from "./routes/route.js";
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.use(cors());
+app.use(express.json());
+
+app.use(routes);
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}/`);
+});
